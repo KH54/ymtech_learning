dc898617f31e11de3e36211dd034a6b9b07ad102
KH54 <pkh543@naver.com>
2021-09-07 15:39:54 +0900

    confirm

--- ymtech-report3/src/Car.java
+++ ymtech-report3/src/Car.java
@@ -1,34 +1,29 @@
-import java.util.Arrays;
 /**
  * Old Model을 New Model로 변환 후 출력
  * 
  * @author "KyungHun Park"
  * @since 2021. 9. 6.
- *
+ * 
  */
 public class Car {
     public static void main(String[] args) {
 
         // model 객체 생성
         OldModel om = new OldModel();
-        NewModel no = new NewModel();
+        NewModel nm = new NewModel();
 
         // Old Model의 필드 출력
-        System.out.printf("Old Model : %d, %s, %.1f, %b, %s \n", 
-                om.getPeople(), om.getDriver(), om.getGasMileage(), om.isPower(), Arrays.toString(om.getPeopleName()));
+        System.out.println(om.toString());
 
         // New Model의 필드 출력
-        System.out.printf("New Model : %d, %s, %.1f, %b, %s \n", 
-                no.getPeople(), no.getDriver(), no.getGasMileage(), no.isPower(), Arrays.toString(no.getPeopleName()));
+        System.out.println(nm.toString());
 
         // Old Model에 New Model 변환
-        om.setPeople(no.getPeople());
-        om.setGasMileage(no.getGasMileage());
-        om.setPeopleName(no.getPeopleName());
-
+        OldModel castOm = new OldModel(nm);
+        NewModel castNm = new NewModel(om);
         // 변환한 om 출력
-        System.out.printf("Old Model : %d, %s, %.1f, %b, %s \n", 
-                om.getPeople(), om.getDriver(), om.getGasMileage(), om.isPower(), Arrays.toString(om.getPeopleName()));
+        System.out.println(castOm.toString());
+        System.out.println(castNm.toString());
     }
 
 }
--- ymtech-report3/src/NewModel.java
+++ ymtech-report3/src/NewModel.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Old Model과 field가 동일한 New Model
  * 
@@ -8,60 +10,92 @@
 public class NewModel {
 
     // field 변수 선언
-    private int people;
+    private int headCount; // 변수이름 변경
     private String driver;
     private double gasMileage;
     private boolean power;
     private String[] peopleName;
 
-    // field 초기화
+    // 기본 생성자
     public NewModel() {
-        this.people = 1;
-        this.driver = "경훈";
-        this.gasMileage = 15.5;
-        this.power = false;
-        this.peopleName = new String[] { "경훈" };
+
+    }
+    
+    // om의 필드를 입력하는 생성자
+    public NewModel(OldModel om) {
+        this.headCount = om.getHeadCount();
+        this.driver = om.getDriver();
+        this.gasMileage = om.getGasMileage();
+        this.power = om.isPower();
+        this.peopleName = om.getPeopleName();
     }
 
-    // getter, setter
-    public int getPeople() {
-        return people;
+    // 사람의 수를 get
+    public int getHeadCount() {
+        return headCount;
+    }
+    
+    // 사람의 수를 set
+    public void setHeadCount(int people) {
+        this.headCount = people;
     }
 
-    public void setPeople(int people) {
-        this.people = people;
-    }
-
+    // 드라이버의 이름을 get
     public String getDriver() {
         return driver;
     }
-
+    
+    // 드라이버의 이름을 set
     public void setDriver(String driver) {
         this.driver = driver;
     }
 
+    // 연비를 get
     public double getGasMileage() {
         return gasMileage;
     }
 
+    // 연비를 set
     public void setGasMileage(double gasMileage) {
         this.gasMileage = gasMileage;
     }
 
+    // 시동 상태를 get
     public boolean isPower() {
         return power;
     }
 
+    // 시동 상태를 set
     public void setPower(boolean power) {
         this.power = power;
     }
 
+    // 탑승한 사람의 이름을 get
     public String[] getPeopleName() {
         return peopleName;
     }
 
+    // 탑승한 사람의 이름을 set
     public void setPeopleName(String[] arr) {
-        this.peopleName = arr;
+        this.peopleName = arr; 
     }
-
+ 
+    // 모든 field를 출력
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("NewModel [people=");
+        builder.append(headCount);
+        builder.append(", driver=");
+        builder.append(driver);
+        builder.append(", gasMileage=");
+        builder.append(gasMileage);
+        builder.append(", power=");
+        builder.append(power);
+        builder.append(", peopleName=");
+        builder.append(Arrays.toString(peopleName));
+        builder.append("]");
+        return builder.toString();
+    }
+    
 }
