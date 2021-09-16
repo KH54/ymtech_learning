c69fdd71878335921f536613af0f2574a1fabbd9
KH54 <pkh543@naver.com>
2021-09-16 15:54:30 +0900

    DAO generic

--- ymtech-report4/src/com/ymtech/board/dao/impl/CommentDao2.java
+++ ymtech-report4/src/com/ymtech/board/dao/impl/CommentDao2.java
@@ -1,8 +1,9 @@
 package com.ymtech.board.dao.impl;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 import com.ymtech.board.dao.ICommentDao;
 import com.ymtech.board.vo.Comment;
@@ -23,88 +24,74 @@ public class CommentDao2 extends GenericDao<Comment, Integer> implements ICommen
      *
      */
     public List<Comment> select() {
-
-        // DB 연결 및 쿼리 호출 실행
-        try {
-            // 상속받은 Generic의 selectAll 메소드의 결과 값을 반환
-            return super.selectAll(DB.SQL_COMMENT_SELECT, (rs) -> {
-                try {
-                    // User에 ResultSet을 매개변수로 하는 생성자의 반환 값을 매개변수로
-                    return new Comment(rs);
-                } catch (SQLException ignor) {
-                    return null;
-                }
-            });
-
-        } catch (SQLException e) {
-            System.out.println("Comment select SQL Error");
-
-        }
-        return null;
+        // 상속받은 Generic의 selectAll 메소드의 결과 값을 반환
+        return super.selectAll(DB.SQL_COMMENT_SELECT, (rs) -> {
+            try {
+                // User에 ResultSet을 매개변수로 하는 생성자의 반환 값을 매개변수로
+                return new Comment(rs);
+            } catch (SQLException ignor) {
+                return null;
+            }
+        });
     }
 
     /**
      * 댓글 작성 메소드
      *
      * @author "KyungHun Park"
-     * @since 2021. 9. 9. 오후 10:17:22
-     *
+     * @since 2021. 9. 16. 오후 1:31:45
+     * @see
      */
     public Integer insert(Comment comment) {
-        // 사용자 입력 값을 저장하기 위한 list
-        List<Object> list = new ArrayList<>();
+        // 상속받은 Generic의 insert 메소드의 결과 값을 반환
+        return super.insert(comment, DB.SQL_COMMENT_INSERT, new BiConsumer<PreparedStatement, Comment>() {
 
-        list.add(comment.getUserId());
-        list.add(comment.getBoardIndex());
-        list.add(comment.getContent());
-        list.add(comment.getParentIndex());
-        try {
-            // 상속받은 Generic의 insert 메소드의 결과 값을 반환
-            return super.insert(DB.SQL_COMMENT_INSERT, list);
-        } catch (SQLException e) {
-            System.out.println("Comment insert SQL Error");
-        }
-        return null;
+            @Override
+            public void accept(PreparedStatement stmt, Comment comment) {
+                try {
+                    stmt.setString(1, comment.getUserId());
+                    stmt.setInt(2, comment.getBoardIndex());
+                    stmt.setString(3, comment.getContent());
+                    stmt.setInt(4, comment.getParentIndex());
+                } catch (SQLException e) {
+                    System.out.println("CommentDao insert accept Error");
+                }
+            }
+        });
     }
 
     /**
      * 댓글 삭제 메소드
      *
      * @author "KyungHun Park"
-     * @since 2021. 9. 9. 오후 10:18:21
-     *
+     * @since 2021. 9. 16. 오후 1:31:40
+     * @see
      */
     public Integer delete(Comment comment) {
-
-        try {
-            // 상속받은 Generic의 delete 메소드의 결과 값을 반환
-            return super.delete(DB.SQL_COMMENT_DELETE, comment.getCommentIndex());
-        } catch (SQLException e) {
-            System.out.println("Comment delete SQL Error");
-        }
-        return null;
+        // 상속받은 Generic의 delete 메소드의 결과 값을 반환
+        return super.delete(DB.SQL_COMMENT_DELETE, comment.getCommentIndex());
     }
 
     /**
      * 댓글 수정 메소드
      *
      * @author "KyungHun Park"
-     * @since 2021. 9. 9. 오후 10:19:20
-     *
+     * @since 2021. 9. 16. 오후 1:31:33
+     * @see
      */
     public Integer update(Comment comment) {
-        // 사용자 입력 값을 저장하기 위한 list
-        List<Object> list = new ArrayList<>();
+        // 상속받은 Generic의 update 메소드의 결과 값을 반환
+        return super.update(comment, DB.SQL_COMMENT_UPDATE, new BiConsumer<PreparedStatement, Comment>() {
 
-        list.add(comment.getContent());
-        list.add(comment.getCommentIndex());
-
-        try {
-            // 상속받은 Generic의 update 메소드의 결과 값을 반환
-            return super.update(DB.SQL_COMMENT_UPDATE, list);
-        } catch (SQLException e) {
-            System.out.println("Board update SQL Error");
-        }
-        return null;
+            @Override
+            public void accept(PreparedStatement stmt, Comment comment) {
+                try {
+                    stmt.setString(1, comment.getContent());
+                    stmt.setInt(2, comment.getCommentIndex());
+                } catch (SQLException e) {
+                    System.out.println("CommentDao update accept Error");
+                }
+            }
+        });
     }
 }
